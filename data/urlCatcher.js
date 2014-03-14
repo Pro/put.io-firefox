549473921f2edb497fbf76319713a9e391d60058
Stefan Profanter <git@s.profanter.me>
2014-03-14 01:06:00 +0100

    Adding transfer working

--- data/urlCatcher.js
+++ data/urlCatcher.js
@@ -1,5 +1,5 @@
 
-prefs = null;
+var prefs = null;
 
 self.port.on("setPrefs", function(prefsPar) {
     prefs = prefsPar;
@@ -14,8 +14,18 @@ var catchableUrl = function(url) {
     return false;
 };
 
+function getParameterByName(url, name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(url);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 window.addEventListener("click", function(event)
 {
+    // only handle left click
+    if (event.which !== 1)
+        return;
     var link = event.target;
     while (link && link.localName != "a")
         link = link.parentNode;
@@ -23,7 +33,22 @@ window.addEventListener("click", function(event)
     // check if URL can be handled by put.io
     if (link && catchableUrl(link.href))
     {
-        self.port.emit("click", link.href, link.title);
+        var title = null;
+        if (link.href.indexOf("magnet:")==0) {
+            var displayName = getParameterByName(link.href, "dn");
+            if (displayName !== null)
+                title = displayName;
+        }
+        if (title === null || title.length === 0) {
+            title = link.title;
+        }
+        if (title === null || title.length === 0) {
+            //if magnet, try to get dn parameter (display name)
+            if (title === null || title.length === 0) {
+                title = link.innerText;
+            }
+        }
+        self.port.emit("click", link.href, title);
         event.preventDefault();
     }
 }, false);
